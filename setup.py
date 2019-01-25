389c122733c71dbe345c2fdeaece5230f9a644cb
Gil Forcada <gil.gnome@gmail.com>
2019-01-25 14:06:49 +0100

    Back to development: 2.1.0.post1
    [ci skip]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = '2.1.0.post0'
+version = '2.1.0.post1.dev0'
 
 setup(name = 'collective.indexing',
       version = version,
